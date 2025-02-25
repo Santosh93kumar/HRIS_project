9775e048f62c29c8804cf63fe2dda366140aecff
santosh <sk8065788@gmail.com>
2025-02-25 14:23:06 +0530

    files

--- HRIS_backend/index.js
+++ HRIS_backend/index.js
@@ -13,9 +13,14 @@ app.use(express.json());
 // Routes
 app.use(mainRouts);
 
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "UP", message: "Server is running smoothly!" });
+});
+
 // Serve static files from 'uploads/EmployeeImage'
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use("/uploads/BalanceImage", express.static("uploads/BalanceImage"));
+
 
 
 
