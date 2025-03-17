cb32f59cc961a3df538645e12e1563612be1e32f
santosh <sk8065788@gmail.com>
2025-03-17 15:55:52 +0530

    new

--- HRIS_project/src/Component/Hrreport/Hrattendancereport.jsx
+++ HRIS_project/src/Component/Hrreport/Hrattendancereport.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Header from "../Header";
-import icon from "../Payroll/print.png";
+import icon from "../Payroll/Print.png";
 
 function HrAttendanceReport() {
   const [filters, setFilters] = useState({
